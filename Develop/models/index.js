c5cf2003de7e9cfbd5b3afe423f483074e7d5530
Jescel James <jesceljames@gmail.com>
2024-01-12 06:10:25 -0500

    mode index added associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,17 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-// Product.belongsTo(Category, {
-//   foreignKey: "category_id"
-// })
+// Product belongsTo Category
+  Product.belongsTo(Category, {
+    foreignKey: "category_id"
+  });
+
 
 // Categories have many Products
-// Category.hasMany
+  Category.hasMany(Product, {
+    foreignKey: "category_id"
+  });
+
 
 // Products belongToMany Tags (through ProductTag)
+  Product.belongsToMany(Tag, {
+    through: ProductTag,
+    foreignKey: "product_id"
+  });
+
 
 // Tags belongToMany Products (through ProductTag)
+  Tag.belongsToMany(Product, {
+    through: ProductTag,
+    foreignKey: "tag_id"
+  });
+
 
 module.exports = {
   Product,
