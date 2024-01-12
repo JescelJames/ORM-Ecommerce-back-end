155f7b03bd7d12c4352fb9920bb1f412876b2983
Jescel James <jesceljames@gmail.com>
2024-01-12 05:49:41 -0500

    product tag model checks good

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,37 +4,37 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-// ProductTag.init(
-//   {
-//     // define columns
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//       allowNull: false
-//     },
-//     product_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "product",
-//         key: "id"
-//       }
-//     },
-//     tag_id: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "tag",
-//         key: "id"
-//       }
-//     }
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'product_tag',
-//   }
-// );
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
 
 module.exports = ProductTag;
