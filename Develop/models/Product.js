55af3358ac6fd060f04a41c68e1d0217cdea9519
Jescel James <jesceljames@gmail.com>
2024-01-12 05:26:14 -0500

    model category added object kv pairs and dataypes

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -7,50 +7,50 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        isNumeric: true
-      }
-    },
-    category_id:
-    {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      reference: {
-        model: 'category',
-        key: 'id'
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+// Product.init(
+//   {
+//     // define columns
+//     id: {
+//       type: DataTypes.INTEGER,
+//       primaryKey: true,
+//       autoIncrement: true,
+//       allowNull: false,
+//     },
+//     product_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     price: {
+//       type: DataTypes.DECIMAL(10, 2),
+//       allowNull: false,
+//       validate: {
+//         isDecimal: true
+//       },
+//     },
+//     stock: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       validate: {
+//         isNumeric: true
+//       }
+//     },
+//     category_id:
+//     {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       reference: {
+//         model: 'category',
+//         key: 'id'
+//       },
+//     },
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'product',
+//   }
+// );
 
 module.exports = Product;
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,9 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id"
-})
+// Product.belongsTo(Category, {
+//   foreignKey: "category_id"
+// })
 
 // Categories have many Products
 // Category.hasMany
