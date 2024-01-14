60d1e48fcd15b06a01367800d183563d9ed5b179
Jescel James <jesceljames@gmail.com>
2024-01-14 11:35:51 -0500

    get by id shows up

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,12 +2,13 @@
   const router = require('express').Router();
   const { Category, Product } = require('../../models');
 
-// ROUTES ___________________________________________
+// CATEGORY ROUTES ___________________________________________
 
   // GET All Categories ENDPOINT: /api/categories
+
     router.get('/', async (req, res) => {
       try {
-            const categoryData = await Category.findAll ({
+            const categoryDataAll = await Category.findAll ({
               include: [
                 {
                   model: Product,
@@ -16,10 +17,9 @@
               ]
 
             })
-            res.json(categoryData);
+            res.json(categoryDataAll);
       } 
-      catch (err) 
-      {
+      catch (err) {
         console.log(err);
         res.status(500).json(err);
       }
@@ -28,28 +28,55 @@
   
     
   // GET By Id ENDPOINT: /api/categories/<insert id number> 
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    router.get('/:id', async (req, res) => {
+      try {
+        const categoryDataId = await Category.findOne({
+          where: {
+            id: req.params.id,
+          },
+          // attributes: ['id', 'category_name'],
+          include: [
+            {
+              model: Product,
+              attributes: ['product_name'],
+            },
+          ],
+
+        });
+        if (!categoryDataId) {
+          res.status(404).json({ message: 'No category found with this id' });
+          return;
+        }
+        res.json(categoryDataId);
+
+      }
+      catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+
+      }
+    });
 
 
-  // find all categories
-  // be sure to include its associated Products
+  // _______________  
+
+    router.post('/', (req, res) => {
+      // create a new category
+    });
+
+  // ________________________  
+
+    router.put('/:id', (req, res) => {
+      // update a category by its `id` value
+    });
 
 
+  // __________________________
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+    router.delete('/:id', (req, res) => {
+      // delete a category by its `id` value
+    });
 
 module.exports = router;
